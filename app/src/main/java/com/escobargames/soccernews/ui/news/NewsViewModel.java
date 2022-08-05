3665d12181ee4507da17ef7f05223212cf88c277
daniloescobar <daniloescobar2006@yahoo.com.br>
2022-08-05 13:19:12 -0300

    Inclusao do retrofit e picasso

--- app/src/main/java/com/escobargames/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/escobargames/soccernews/ui/news/NewsViewModel.java
@@ -4,25 +4,49 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.escobargames.soccernews.data.remote.SoccerNewsApi;
 import com.escobargames.soccernews.domain.News;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class NewsViewModel extends ViewModel {
 
-    private MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        this.news = new MutableLiveData<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://daniloescobar.github.io/soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        //TODO Remover mock de noticias
-        List<News> news = new ArrayList<>();
-        news.add(new News("Ferroviaria tem desfalques importantes", "Time feminino da ferroviaria vem com desfalques para o jogo contra o Corinthians"));
-        news.add(new News("São Paulo se classifica para a semifinal da Copa do Brasil", "São Paulo ganha novamente do America-MG e avança para as semifinais da Copa do Brasil"));
-        news.add(new News("Atletico-MG empata com Palmeiras, mas se classifica nos penaltis", "Em grande jogo no Allianz Parque, os dois times empatam e o galo ganha nos penaltis"));
+       api = retrofit.create(SoccerNewsApi.class);
+        this.findNews();
+    }
 
-        this.news.setValue(news);
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if(response.isSuccessful()){
+                    news.setValue(response.body());
+                }else{
+                    //TODO tratar erros
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                //TODO tratar erros
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
